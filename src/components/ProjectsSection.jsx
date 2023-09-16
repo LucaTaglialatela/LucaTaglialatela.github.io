fb0c03262772891d57b5476b9c697ae1c9ea2d20
LucaTaglialatela <45466948+LucaTaglialatela@users.noreply.github.com>
2023-09-17 03:09:54 +0800

    reduce img size

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Project from "./Project";
+import { useEffect } from "react";
 
 const ProjectList = styled.div`
     display: grid;
@@ -10,12 +11,12 @@ const ProjectList = styled.div`
 function ProjectsSection() {
     // const imgPath = "../public/";
     const imgPath = "./";
-
+    
     return (
         <ProjectList>
             <Project
                 title={"Alice in Gravityland"}
-                image={`${imgPath}alice_in_gravityland_teaser.png`}
+                image={`${imgPath}alice_in_gravityland_teaser.jpg`}
                 summary={"Alice in Gravityland is a virtual reality experience exploring gravity with novel, around-the-head vibrotactile feedback using illusory tactile motion. The haptic feedback helps improve players\’ sense of directionality to increase their level of immersion. Inspired by Lewis Carroll\’s Alice\’s Adventures in Wonderland (1865), this multi-sensory adventure invites players to alter gravity to solve puzzles and experience gravity in a unique way."}
                 skills={[ "Unity", "C#", "Arduino", "Git" ]}
                 description={"Alice in Gravityland is a virtual reality experience exploring gravity with novel, around-the-head vibrotactile feedback using illusory tactile motion. The haptic feedback helps improve players\’ sense of directionality to increase their level of immersion. Inspired by Lewis Carroll\’s Alice\’s Adventures in Wonderland (1865), this multi-sensory adventure invites players to alter gravity to solve puzzles and experience gravity in a unique way."}
@@ -26,6 +27,20 @@ function ProjectsSection() {
                 summary={"Project Description"}
                 skills={[ "Python", "OpenCV", "Ubuntu", "Git" ]}
                 description={""}
+            />
+            <Project
+                title={"Alice in Gravityland"}
+                image={`${imgPath}alice_in_gravityland_teaser.jpg`}
+                summary={"Alice in Gravityland is a virtual reality experience exploring gravity with novel, around-the-head vibrotactile feedback using illusory tactile motion. The haptic feedback helps improve players\’ sense of directionality to increase their level of immersion. Inspired by Lewis Carroll\’s Alice\’s Adventures in Wonderland (1865), this multi-sensory adventure invites players to alter gravity to solve puzzles and experience gravity in a unique way."}
+                skills={[ "Unity", "C#", "Arduino", "Git" ]}
+                description={"Alice in Gravityland is a virtual reality experience exploring gravity with novel, around-the-head vibrotactile feedback using illusory tactile motion. The haptic feedback helps improve players\’ sense of directionality to increase their level of immersion. Inspired by Lewis Carroll\’s Alice\’s Adventures in Wonderland (1865), this multi-sensory adventure invites players to alter gravity to solve puzzles and experience gravity in a unique way."}
+            />
+            <Project
+                title={"Alice in Gravityland"}
+                image={`${imgPath}alice_in_gravityland_teaser.jpg`}
+                summary={"Alice in Gravityland is a virtual reality experience exploring gravity with novel, around-the-head vibrotactile feedback using illusory tactile motion. The haptic feedback helps improve players\’ sense of directionality to increase their level of immersion. Inspired by Lewis Carroll\’s Alice\’s Adventures in Wonderland (1865), this multi-sensory adventure invites players to alter gravity to solve puzzles and experience gravity in a unique way."}
+                skills={[ "Unity", "C#", "Arduino", "Git" ]}
+                description={"Alice in Gravityland is a virtual reality experience exploring gravity with novel, around-the-head vibrotactile feedback using illusory tactile motion. The haptic feedback helps improve players\’ sense of directionality to increase their level of immersion. Inspired by Lewis Carroll\’s Alice\’s Adventures in Wonderland (1865), this multi-sensory adventure invites players to alter gravity to solve puzzles and experience gravity in a unique way."}
             />
         </ProjectList>
     );
