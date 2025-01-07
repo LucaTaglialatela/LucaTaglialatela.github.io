62da2da32eecf40b4b57d152234c3d5c17aca7f6
LucaTaglialatela <luca.taglialatela@gmail.com>
2025-01-07 19:27:52 +0900

    add svg icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import { LinkedIn } from "./components/icons/LinkedInIcon";
+import { GitHub } from "./components/icons/GitHub";
+import { Orcid } from "./components/icons/Orcid";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   const header = (
     <div
-      className="px-14 flex items-center justify-between sticky top-0 z-20 text-base font-bold"
+      className="px-14 flex items-center justify-end sticky top-0 z-20 text-base font-bold gap-3"
       style={{
         height: `calc(45px + 3.6rem)`,
         background: "var(--bg-opaque)",
         backdropFilter: "blur(12px)",
       }}
     >
-      asdasdasd
+      <a href={"https://www.linkedin.com/in/lucataglialatela/"}>
+        <LinkedIn className="w-8 h-8 text-gray-200" />
+      </a>
+      <a href={"https://github.com/LucaTaglialatela/"}>
+        <GitHub className="w-8 h-8 text-gray-200" />
+      </a>
+      <a href={"https://orcid.org/0009-0000-2048-6691/"}>
+        <Orcid className="w-8 h-8 text-gray-200" />
+      </a>
     </div>
   );
 
